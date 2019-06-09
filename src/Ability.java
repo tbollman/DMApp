e42f072a11fe5907f948c9287e0bc08ba2748588
askewotherwise <natalie.askew@gmail.com>
2019-06-09 14:49:33 -0400

    changed default constructor

--- src/Ability.java
+++ src/Ability.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Ability {
+
+    private int abilityScore;
+    private int abilityModifier;
+
+    //default constructor
+    public Ability() {
+        this.setAbilityScore(10);
+    }
+
+    //setters and getters
+    public void setAbilityScore(int inputScore) {
+        this.abilityScore = inputScore;
+    }
+
+    public int getAbilityScore() {
+        return this.abilityScore;
+    }
+
+    public int getAbilityModifier() {
+        this.abilityModifier = (this.abilityScore/2) - 5;
+        return abilityModifier;
+    }
+
 }
