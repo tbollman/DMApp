b0d15acc794dd75bbc7dfee0e430c00f440c1036
askewotherwise <natalie.askew@gmail.com>
2019-06-09 15:00:04 -0400

    made changes to driver

--- src/CharacterDriver.java
+++ src/CharacterDriver.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class CharacterDriver {
+    public static void main(String[] args) {
+        System.out.println("Character Driver");
+        Scanner keyboard = new Scanner(System.in);
+
+        CharacterSheet quintus = new CharacterSheet();
+
+        quintus.characterCreator();
+
+        quintus.displayCharacterInfo();
+
+    }  // end of main method
 }
