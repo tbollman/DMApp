a412571b7b5b32e60fa565d5ed79917a96f936a2
tbollman <47295560+tbollman@users.noreply.github.com>
2019-06-13 15:32:18 -0400

    Added characterSheet read function

--- UserMan.java
+++ UserMan.java
@@ -11,6 +11,8 @@ public class UserMan {
   public int account_index;
   public String user_name;
   public boolean logged_in = false;
+  LinkedList<CharacterSheet> characters = new LinkedList<CharacterSheet>();
+  public int character_count;
 
   public void read_file() {
     JSONParser jsonParser = new JSONParser();
@@ -25,6 +27,100 @@ public class UserMan {
             users.add(new User(username, account_type, password));
             user_count++;
           }
+    }
+    catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+  public void read_characters() {
+    JSONParser jsonParser = new JSONParser();
+    try {
+      JSONArray characterList = (JSONArray)jsonParser.parse(new FileReader("./src/character_sheet.json"));
+      for (Object o : characterList) {
+        JSONObject character = (JSONObject) o;
+        String username = (String) character.get("username");
+        if(username.equals(user_name)) {
+          String characterName = (String) character.get("Character Name");
+          String characterRace = (String) character.get("Character Race");
+          String characterClass = (String) character.get("Character Class");
+          String background = (String) character.get("Background");
+          int strScore = Integer.parseInt((String) character.get("Strength Score"));
+          int dexScore = Integer.parseInt((String) character.get("Dexterity Score"));
+          int conScore = Integer.parseInt((String) character.get("Constitution Score"));
+          int intScore = Integer.parseInt((String) character.get("Intelligence Score"));
+          int wisScore = Integer.parseInt((String) character.get("Wisdom Score"));
+          int chaScore = Integer.parseInt((String) character.get("Charisma Score"));
+
+          int armorClass = Integer.parseInt((String) character.get("Armor Class"));
+          int initiative = Integer.parseInt((String) character.get("Initiative"));
+          int speed = Integer.parseInt((String) character.get("Speed"));
+          String hitDie = (String) character.get("Hit Die");
+          int hitPoints = Integer.parseInt((String) character.get("Hit Points"));
+          int proficiency = Integer.parseInt((String) character.get("Proficiency Bonus"));
+          LinkedList<Integer> skills = new LinkedList<Integer>();
+          int strengthSave = Integer.parseInt((String) character.get("Strength Save"));
+          skills.add(strengthSave);
+          int dexSave = Integer.parseInt((String) character.get("Dexterity Save"));
+          skills.add(dexSave);
+          int constitutionSave = Integer.parseInt((String) character.get("Constitution Save"));
+          skills.add(constitutionSave);
+          int intelligenceSave = Integer.parseInt((String) character.get("Intelligence Save"));
+          skills.add(intelligenceSave);
+          int wisdomSave = Integer.parseInt((String) character.get("Wisdom Save"));
+          skills.add(wisdomSave);
+          int charismaSave = Integer.parseInt((String) character.get("Charisma Save"));
+          skills.add(charismaSave);
+          int acrobatics = Integer.parseInt((String) character.get("Acrobatics"));
+          skills.add(acrobatics);
+          int animal_handling = Integer.parseInt((String) character.get("Animal Handling"));
+          skills.add(animal_handling);
+          int arcana = Integer.parseInt((String) character.get("Arcana"));
+          skills.add(arcana);
+          int athletics = Integer.parseInt((String) character.get("Athletics"));
+          skills.add(athletics);
+          int deception = Integer.parseInt((String) character.get("Deception"));
+          skills.add(deception);
+          int history = Integer.parseInt((String) character.get("History"));
+          skills.add(history);
+          int insight = Integer.parseInt((String) character.get("Insight"));
+          skills.add(insight);
+          int intimidation = Integer.parseInt((String) character.get("Intimidation"));
+          skills.add(intimidation);
+          int investigation = Integer.parseInt((String) character.get("Investigation"));
+          skills.add(investigation);
+          int medicine = Integer.parseInt((String) character.get("Medicine"));
+          skills.add(medicine);
+          int nature = Integer.parseInt((String) character.get("Nature"));
+          skills.add(nature);
+          int perception = Integer.parseInt((String) character.get("Perception"));
+          skills.add(perception);
+          int performance = Integer.parseInt((String) character.get("Performance"));
+          skills.add(performance);
+          int persuasion = Integer.parseInt((String) character.get("Persuasion"));
+          skills.add(persuasion);
+          int religion = Integer.parseInt((String) character.get("Religion"));
+          skills.add(religion);
+          int sleightOfHand = Integer.parseInt((String) character.get("Sleight of Hand"));
+          skills.add(sleightOfHand);
+          int stealth = Integer.parseInt((String) character.get("Stealth"));
+          skills.add(stealth);
+          int survival = Integer.parseInt((String) character.get("Survival"));
+          skills.add(survival);
+          LinkedList<String> languages = (LinkedList<String>) character.get("Languages");
+          LinkedList<String> features = (LinkedList<String>) character.get("Features");
+          LinkedList<String> weapons = (LinkedList<String>) character.get("Weapons");
+          LinkedList<String> armor = (LinkedList<String>) character.get("Armor");
+          LinkedList<String> potions = (LinkedList<String>) character.get("Potions");
+          LinkedList<String> supplies = (LinkedList<String>) character.get("Supplies");
+          LinkedList<String> spells = (LinkedList<String>) character.get("Spells");
+
+          characters.add(new
+          CharacterSheet(user_name,
+            characterName,characterRace,characterClass,background,strScore,dexScore,conScore,intScore,
+            wisScore,chaScore,armorClass,initiative,speed,hitDie,hitPoints,proficiency,skills,languages,features,weapons,armor,potions,supplies,spells));
+          character_count++;
+        }
+      }
     }
     catch (Exception e) {
       System.out.println(e);
