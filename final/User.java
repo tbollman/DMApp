c311325434493613110380e18babb62eecefcd75
tbollman <47295560+tbollman@users.noreply.github.com>
2019-06-20 15:53:39 -0400

    Update User.java

--- final/User.java
+++ final/User.java
@@ -1,31 +1,40 @@
-public class User {
+public class User 
+{
     private String userName;
     private String accountType;
     private String password;
-    public User() {
+    public User() 
+    {
         userName = accountType = password = "none";
     }
-    public User(String a_user_name, String an_account_type, String a_password) {
+    public User(String a_user_name, String an_account_type, String a_password) 
+    {
         setUserName(a_user_name);
         setAccountType(an_account_type);
         setPassword(a_password);
     }
-    public String getUserName() {
+    public String getUserName() 
+    {
         return userName;
     }
-    public String getAccountType() {
+    public String getAccountType() 
+    {
         return accountType;
     }
-    public String getPassword() {
+    public String getPassword() 
+    {
         return password;
     }
-    public void setUserName(String aUserName) {
+    public void setUserName(String aUserName) 
+    {
         userName = aUserName;
     }
-    public void setAccountType(String anAccountType) {
+    public void setAccountType(String anAccountType) 
+    {
         accountType = anAccountType;
     }
-    public void setPassword(String aPassword) {
+    public void setPassword(String aPassword) 
+    {
         password = aPassword;
     }
 }
