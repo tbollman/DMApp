477e5e9f233523caa635790b4816d6d755511e93
tbollman <47295560+tbollman@users.noreply.github.com>
2019-06-20 23:23:02 -0400

    Updated tests to include broader coverage

--- test/UserManTest.java
+++ test/UserManTest.java
@@ -12,17 +12,57 @@ class UserManTest {
         users.readFile();
         int actual = users.userCount;
         assertEquals(7,actual, "7 Users Exist!");
+        String actualName = users.get(0).getUserName();
+        String actualPassword = users.get(0).getUserName();
+        String actualType = users.get(0).getAccountType();
+        assertEquals("tbollman", actualName, "First account is tbollman");
+        assertEquals("player", actualType, "tbollman is a player");
+        assertEquals("password7", actualPassword, "password is password7");
     }
 
     @org.junit.jupiter.api.Test
     void readCharacters() {
         users.userName = "tbollman";
         users.readCharacters();
+        String actualCN = users.characters.get(0).getCharacterName();
+        assertEquals("Quintus Metellus", actualCN, "Character 0 name is Quintus Metellus");
+        int actualLvl = users.characters.get(0).getLevel();
+        assertEquals(0,actualLvl, "Quintus' Level is 0");
+        String actualCC = users.characters.get(0).getCharacterClass();
+        assertEquals("Fighter", actualCC, "Character 0 class if fighter");
+        String actualCR = users.characters.get(0).getCharacterRace();
+        assertEquals("Human", actualCR, "Character 0 race is Human");
+        String actualBg = users.characters.get(0).getBackground();
+        assertEquals("City Watch", actualBg, "Character 0 background is City Watch");
+        int actualAC = users.characters.get(0).getArmorClass();
+        assertEquals(10, actualAC, "Character 0 Armor Class is 10");
+        int actualInit = users.characters.get(0).getInitiative();
+        assertEquals(0, actualInit, "Character 0 name is 0");
+        int actualSpeed = users.characters.get(0).getSpeed();
+        assertEquals(30, actualSpeed, "Character 0 speed is 30);
+        int actualSS = users.characters.get(0).getStrengthScore();
+        assertEquals(10, actualSS, "Character 0 score is 10);
+        int actualDS = users.characters.get(0).getDexterityScore();
+        assertEquals(10, actualDS, "Character 0 score is 10);
+        int actualCoS = users.characters.get(0).getConstitutionScore();
+        assertEquals(10, actualCoS, "Character 0 score is 10);
+        int actualIS = users.characters.get(0).getIntelligenceScore();
+        assertEquals(10, actualIS, "Character 0 score is 10);
+        int actualWS = users.characters.get(0).getWisdomScore();
+        assertEquals(10, actualWS, "Character 0 score is 10);
+        int actualChS = users.characters.get(0).getCharismaScore();
+        assertEquals(10, actualChS, "Character 0 score is 10);
+        int actualProS = users.characters.get(0).getProficiencyBonus();
+        assertEquals(10, actualProS, "Character 0 proficiency is 10);
+        for(int i = 0; i < users.get(0).skill.size(); i++)
+        {
+            actual = users.get(0).getSkill(i);
+            assertEquals(0, actual, "Skill "+i+"'s value is 0");
+        }
         int actual = users.ownedCharacterCount;
         assertEquals(2, actual, "tbollman has 2 characters");
         String actualName = users.ownedCharacters.get(users.ownedCharacterCount-1).getCharacterName();
         assertEquals("Test Character", actualName, "Last owned characters name is Test Character");
-
     }
 
     @org.junit.jupiter.api.Test
@@ -32,8 +72,10 @@ class UserManTest {
         users.storeValues(test,actual);
         String ExpectedOne = "Tom";
         String ExpectedTwo = "Dick";
+        String ExpectedThree = "Harry";
         assertEquals(ExpectedOne, actual.get(0), "The first String is: Tom");
         assertEquals(ExpectedTwo, actual.get(1), "The second String is: Dick");
+        assertEquals(ExpectedThree, actual.get(2), "The second String is: Harry");
     }
 
     @org.junit.jupiter.api.Test
